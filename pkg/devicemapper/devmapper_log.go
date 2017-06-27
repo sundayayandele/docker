ae1d6c5c498e9ae48e95fe586964c7eedaffe73f
Aleksa Sarai <asarai@suse.de>
2017-06-28 01:46:47 +1000

    devicemapper: remove LogInit and move all callbacks to pkg
    LogInit was only used to split up the logic of pkg/devicemapper into
    daemon/graphdriver/devmapper. Combine the two so that pkg/devicemapper
    entirely takes care of the log filtering by level as well as forwarding
    to logrus. This makes the code substantially easier to read.
    
    Signed-off-by: Aleksa Sarai <asarai@suse.de>

--- pkg/devicemapper/devmapper_log.go
+++ pkg/devicemapper/devmapper_log.go
@@ -5,17 +5,31 @@ package devicemapper
 import "C"
 
 import (
+	"fmt"
 	"strings"
+
+	"github.com/Sirupsen/logrus"
 )
+
+// dmLogLevel gates what level of libdm's debugging output will be forwarded
+// through logrus. libdm uses logging *very* liberally and most people using
+// --debug [or Docker] aren't going to be debugging libdm bugs. You can change
+// this through --storage-opt="dm.libdm_log_level=X".
+var dmLogLevel = LogLevelFatal
 
 // Due to the way cgo works this has to be in a separate file, as devmapper.go has
 // definitions in the cgo block, which is incompatible with using "//export"
 
-// DevmapperLogCallback exports the devmapper log callback for cgo.
+// DevmapperLogCallback exports the devmapper log callback for cgo. Note that
+// because we are using callbacks, this function will be called for *every* log
+// in libdm (even debug ones because there's no way of setting the verbosity
+// level for an external logging callback).
 //export DevmapperLogCallback
 func DevmapperLogCallback(level C.int, file *C.char, line C.int, dmErrnoOrClass C.int, message *C.char) {
 	msg := C.GoString(message)
-	if level < 7 {
+
+	// Track what errno libdm saw, because the library only gives us 0 or 1.
+	if level < LogLevelDebug {
 		if strings.Contains(msg, "busy") {
 			dmSawBusy = true
 		}
@@ -29,7 +43,39 @@ func DevmapperLogCallback(level C.int, file *C.char, line C.int, dmErrnoOrClass 
 		}
 	}
 
-	if dmLogger != nil {
-		dmLogger.DMLog(int(level), C.GoString(file), int(line), int(dmErrnoOrClass), msg)
+	if int(level) <= dmLogLevel {
+		// Forward the log to the correct logrus level, if allowed by dmLogLevel.
+		logMsg := fmt.Sprintf("libdevmapper(%d): %s:%d (%d) %s", int(level), C.GoString(file), int(line), int(dmErrnoOrClass), msg)
+		switch level {
+		case LogLevelFatal, LogLevelErr:
+			logrus.Error(logMsg)
+		case LogLevelWarn:
+			logrus.Warn(logMsg)
+		case LogLevelNotice, LogLevelInfo:
+			logrus.Info(logMsg)
+		case LogLevelDebug:
+			logrus.Debug(logMsg)
+		default:
+			// Don't drop any "unknown" levels.
+			logrus.Info(logMsg)
+		}
 	}
+}
+
+// registerLogCallback registers our own logging callback function for libdm.
+//
+// Because libdm only gives us {0,1} error codes we need to parse the logs
+// produced by libdm (to set dmSawBusy and so on). Note that by registering a
+// callback using DevmapperLogCallback, libdm will no longer output logs to
+// stderr so we have to log everything ourselves. None of this handling is
+// optional because we depend on log callbacks to parse the logs, and if we
+// don't forward the log information we'll be in a lot of trouble when
+// debugging things.
+func registerLogCallback() {
+	LogWithErrnoInit()
+}
+
+func init() {
+	// Register as early as possible so we don't miss anything.
+	registerLogCallback()
 }
